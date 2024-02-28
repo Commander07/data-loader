c86b8a447f0e984072ca450fd365db8edb7671c6
Commander07 <45269106+Commander07@users.noreply.github.com>
2024-02-28 15:08:05 +0100

    fix: server crashing on start

--- src/main/java/dataloader/mixin/MinecraftServerMixin.java
+++ src/main/java/dataloader/mixin/MinecraftServerMixin.java
@@ -2,10 +2,11 @@ package dataloader.mixin;
 
 import com.google.common.collect.ImmutableSet;
 import dataloader.DataLoader;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resource.*;
 import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,7 +25,7 @@ public class MinecraftServerMixin {
 						DataLoader.DATAPACKS_PATH,
 						ResourceType.SERVER_DATA,
 						DataLoader.RESOURCE_PACK_SOURCE,
-						MinecraftClient.getInstance().getSymlinkFinder()
+						LevelStorage.createSymlinkFinder(FabricLoader.getInstance().getGameDir().resolve(LevelStorage.ALLOWED_SYMLINKS_FILE_NAME))
 				)
 		);
 	}
